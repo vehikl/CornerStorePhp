bff0d51b97d4c603356652fe0c5d81ad30e520cd
Vehikl Team <go@vehikl.com>
2022-02-11 07:56:18 -0500

    extract method

--- src/CornerStore.php
+++ src/CornerStore.php
@@ -15,49 +15,54 @@ final class CornerStore
     public function updateInventory()
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Twinkie' and $item->name != 'Lotto Ticket') {
-                if ($item->price > 0) {
-                    if ($item->name != 'Beef Jerky') {
-                        $item->price = $item->price - 1;
+            $this->updateItem($item);
+        }
+    }
+
+    protected function updateItem(mixed $item): void
+    {
+        if ($item->name != 'Twinkie' and $item->name != 'Lotto Ticket') {
+            if ($item->price > 0) {
+                if ($item->name != 'Beef Jerky') {
+                    $item->price = $item->price - 1;
+                }
+            }
+        } else {
+            if ($item->price < 50) {
+                $item->price = $item->price + 1;
+                if ($item->name == 'Lotto Ticket') {
+                    if ($item->days_until_expired < 11) {
+                        if ($item->price < 50) {
+                            $item->price = $item->price + 1;
+                        }
                     }
+                    if ($item->days_until_expired < 6) {
+                        if ($item->price < 50) {
+                            $item->price = $item->price + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if ($item->name != 'Beef Jerky') {
+            $item->days_until_expired = $item->days_until_expired - 1;
+        }
+
+        if ($item->days_until_expired < 0) {
+            if ($item->name != 'Twinkie') {
+                if ($item->name != 'Lotto Ticket') {
+                    if ($item->price > 0) {
+                        if ($item->name != 'Beef Jerky') {
+                            $item->price = $item->price - 1;
+                        }
+                    }
+                } else {
+                    $item->price = 0;
                 }
             } else {
                 if ($item->price < 50) {
                     $item->price = $item->price + 1;
-                    if ($item->name == 'Lotto Ticket') {
-                        if ($item->days_until_expired < 11) {
-                            if ($item->price < 50) {
-                                $item->price = $item->price + 1;
-                            }
-                        }
-                        if ($item->days_until_expired < 6) {
-                            if ($item->price < 50) {
-                                $item->price = $item->price + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if ($item->name != 'Beef Jerky') {
-                $item->days_until_expired = $item->days_until_expired - 1;
-            }
-
-            if ($item->days_until_expired < 0) {
-                if ($item->name != 'Twinkie') {
-                    if ($item->name != 'Lotto Ticket') {
-                        if ($item->price > 0) {
-                            if ($item->name != 'Beef Jerky') {
-                                $item->price = $item->price - 1;
-                            }
-                        }
-                    } else {
-                        $item->price = 0;
-                    }
-                } else {
-                    if ($item->price < 50) {
-                        $item->price = $item->price + 1;
-                    }
                 }
             }
         }
